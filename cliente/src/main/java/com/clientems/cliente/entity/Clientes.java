acee3f4ed77a8bcf411a2a6c2eda9f3f573e0541
Guilherme Macedo <macedo.lac157@gmail.com>
2024-11-27 00:07:18 -0300

    entidade clientes mapeada

--- cliente/src/main/java/com/clientems/cliente/entity/Clientes.java
+++ cliente/src/main/java/com/clientems/cliente/entity/Clientes.java
@@ -1,7 +1,37 @@
 package com.clientems.cliente.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Entity
+@Table(name = "CLIENTE_TB")
+@AllArgsConstructor @NoArgsConstructor
+@Getter
+@Setter
 public class Clientes {
 	
+	@Id
+	@Column(name = "ID_CLIENTE")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 	
+	@Column(name = "NOME")
+	private String nome;
+	
+	@Column(name = "SOBRENOME")
+	private String sobreNome;
+	
+	@Column(name = "CPF")
+	private String cpf;
 
 }
