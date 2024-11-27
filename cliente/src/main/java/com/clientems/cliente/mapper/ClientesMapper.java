e848f8a1f81c8e257c6234142fa280d23400d287
Guilherme Macedo <macedo.lac157@gmail.com>
2024-11-27 10:33:25 -0300

    Classe ClientesMapper mapeada

--- cliente/src/main/java/com/clientems/cliente/mapper/ClientesMapper.java
+++ cliente/src/main/java/com/clientems/cliente/mapper/ClientesMapper.java
@@ -1,5 +1,62 @@
 package com.clientems.cliente.mapper;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.clientems.cliente.dto.ClientesDTO;
+import com.clientems.cliente.entity.Clientes;
+
+@Service
 public class ClientesMapper {
+
+	public ClientesDTO convertEntityToDto(Clientes dado) {
+		ClientesDTO dto = new ClientesDTO();
+
+		if (dado != null) {
+			dto.setId(dado.getId());
+			dto.setNome(dado.getNome());
+			dto.setSobreNome(dado.getSobreNome());
+			dto.setCpf(dado.getCpf());
+		}
+		return dto;
+	}
+
+	public Clientes convertDtoToEntity(ClientesDTO dto) {
+		Clientes entity = new Clientes();
+
+		if (dto != null) {
+			entity.setId(dto.getId());
+			entity.setNome(dto.getNome());
+			entity.setSobreNome(dto.getSobreNome());
+			entity.setCpf(dto.getCpf());
+		}
+		return entity;
+	}
+
+	public List<ClientesDTO> convertListEntityToListDto(List<Clientes> listaDado) {
+		List<ClientesDTO> listDto = new ArrayList<>();
+
+		if (listDto != null) {
+			for (Clientes dado : listaDado) {
+				listDto.add(convertEntityToDto(dado));
+			}
+		}
+		return listDto;
+
+	}
+
+	public List<Clientes> convertListDtoToListEntity(List<ClientesDTO> listaDto) {
+		List<Clientes> listDado = new ArrayList<>();
+
+		if (listaDto != null) {
+			for (ClientesDTO dado : listaDto) {
+				listDado.add(convertDtoToEntity(dado));
+			}
+		}
+
+		return listDado;
+	}
 
 }
