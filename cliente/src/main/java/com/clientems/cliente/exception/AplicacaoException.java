628bb0bef83d90aacebe69601102e0eab26db550
Guilherme Macedo <macedo.lac157@gmail.com>
2024-11-27 14:23:43 -0300

    mapeamento da classe exception

--- cliente/src/main/java/com/clientems/cliente/exception/AplicacaoException.java
+++ cliente/src/main/java/com/clientems/cliente/exception/AplicacaoException.java
@@ -1,5 +1,17 @@
 package com.clientems.cliente.exception;
 
-public class AplicacaoException {
+public class AplicacaoException extends RuntimeException{
+	
+	private static final long serialVersionUID = 1L;
+
+	
+	public AplicacaoException() {}
+
+	
+	public AplicacaoException(String message)
+    {
+       super(message);
+    }
 
 }
+
