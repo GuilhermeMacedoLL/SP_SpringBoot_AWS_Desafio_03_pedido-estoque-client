6298fcfc07ccc4fdc37c0b92d25495455fffd0c2
Guilherme Macedo <macedo.lac157@gmail.com>
2024-11-28 14:30:14 -0300

    Mapeamento da classe EstoqueService

--- estoque/src/main/java/com/estoquems/estoque/service/EstoqueService.java
+++ estoque/src/main/java/com/estoquems/estoque/service/EstoqueService.java
@@ -1,5 +1,20 @@
 package com.estoquems.estoque.service;
 
-public class EstoqueService {
+import java.util.List;
+
+import com.estoquems.estoque.dto.EstoqueDTO;
+import com.estoquems.estoque.entity.Estoque;
+
+public interface EstoqueService {
+	
+	List<Estoque> getAll();
+	
+	Estoque getEstoque(Long id);
+	
+	Estoque create(EstoqueDTO dtoInput);
+	
+	Estoque update(EstoqueDTO dtoInput);
+	
+	void delete(Long id);
 
 }
