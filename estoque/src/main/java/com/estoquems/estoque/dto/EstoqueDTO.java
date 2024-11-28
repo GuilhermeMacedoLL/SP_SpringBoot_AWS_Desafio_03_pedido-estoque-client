a3ece4461298491fef8c30a9f5d4f451c32e6497
Guilherme Macedo <macedo.lac157@gmail.com>
2024-11-28 14:27:01 -0300

    Mapeamento da classe EstoqueDTO

--- estoque/src/main/java/com/estoquems/estoque/dto/EstoqueDTO.java
+++ estoque/src/main/java/com/estoquems/estoque/dto/EstoqueDTO.java
@@ -1,5 +1,20 @@
 package com.estoquems.estoque.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor @NoArgsConstructor
 public class EstoqueDTO {
 
+	private Long id;
+	private int quantidade;
+	private String nomeProduto;
 }
