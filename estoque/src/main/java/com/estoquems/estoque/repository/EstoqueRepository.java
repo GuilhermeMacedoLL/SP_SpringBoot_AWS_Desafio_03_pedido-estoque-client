d580e8356434e95a393d5ff7b5da23dc547d4daf
Guilherme Macedo <macedo.lac157@gmail.com>
2024-11-28 14:49:03 -0300

    Mapeamento da classe EstoqueRepository

--- estoque/src/main/java/com/estoquems/estoque/repository/EstoqueRepository.java
+++ estoque/src/main/java/com/estoquems/estoque/repository/EstoqueRepository.java
@@ -1,5 +1,15 @@
 package com.estoquems.estoque.repository;
 
-public interface EstoqueRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.estoquems.estoque.entity.Estoque;
+
+public interface EstoqueRepository extends JpaRepository<Estoque, Long>, JpaSpecificationExecutor<Estoque> {
+
+	@Query(value = "SELECT * FROM ESTOQUE WHERE NOME_PRODUTO=:nomeProduto", nativeQuery = true)
+	Estoque getEstoque(@Param("nomeProduto") String nomeProduto);
 
 }
