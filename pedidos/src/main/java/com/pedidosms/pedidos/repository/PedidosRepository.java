da73f2112ec1eb930dd6d6425761aa85a8086b96
Guilherme Macedo <macedo.lac157@gmail.com>
2024-11-29 17:49:13 -0300

    Mapeamento da classe PedidosRepository

--- pedidos/src/main/java/com/pedidosms/pedidos/repository/PedidosRepository.java
+++ pedidos/src/main/java/com/pedidosms/pedidos/repository/PedidosRepository.java
@@ -1,5 +1,15 @@
 package com.pedidosms.pedidos.repository;
 
-public class PedidosRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.pedidosms.pedidos.entity.Pedidos;
+
+public interface PedidosRepository extends JpaRepository<Pedidos, Long>, JpaSpecificationExecutor<Pedidos>{
+
+	@Query(value = "SELECT * FROM PEDIDOS WHERE PEDIDOS_ID=:id", nativeQuery = true)
+	Pedidos getPedidos(@Param("PEDIDOS_ID") Long id);
+	
 }
