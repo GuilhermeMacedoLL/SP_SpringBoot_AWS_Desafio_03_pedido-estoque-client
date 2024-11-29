a3626a5dc5d761bd1f610a7e49729ef8698934e9
Guilherme Macedo <macedo.lac157@gmail.com>
2024-11-29 18:03:02 -0300

    Classe Exception criada e mapeada

--- pedidos/src/main/java/com/pedidosms/pedidos/exception/AplicacaoException.java
+++ pedidos/src/main/java/com/pedidosms/pedidos/exception/AplicacaoException.java
@@ -1,5 +1,14 @@
 package com.pedidosms.pedidos.exception;
 
-public class AplicacaoException {
+public class AplicacaoException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public AplicacaoException() {
+	}
+
+	public AplicacaoException(String message) {
+		super(message);
+	}
 
 }
