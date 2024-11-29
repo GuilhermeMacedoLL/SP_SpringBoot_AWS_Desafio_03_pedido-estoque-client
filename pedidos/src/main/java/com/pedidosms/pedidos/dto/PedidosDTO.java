460bb57d7355e9509a88e01feaa944b3a0bc840e
Guilherme Macedo <macedo.lac157@gmail.com>
2024-11-29 17:41:10 -0300

    Mapeamento da classe PeddidosDTO

--- pedidos/src/main/java/com/pedidosms/pedidos/dto/PedidosDTO.java
+++ pedidos/src/main/java/com/pedidosms/pedidos/dto/PedidosDTO.java
@@ -1,5 +1,23 @@
 package com.pedidosms.pedidos.dto;
 
+import com.pedidosms.pedidos.enums.SituacaoPedido;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor @NoArgsConstructor
 public class PedidosDTO {
+	
+	private long id;
+	private SituacaoPedido situacaoPedido;
+	private Long clienteId;
 
 }
