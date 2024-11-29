869fecfe7dfd81b7258edc0124f6f2d81cac5b63
Guilherme Macedo <macedo.lac157@gmail.com>
2024-11-29 17:28:47 -0300

    Mapeamento da classe Pedidos

--- pedidos/src/main/java/com/pedidosms/pedidos/entity/Pedidos.java
+++ pedidos/src/main/java/com/pedidosms/pedidos/entity/Pedidos.java
@@ -1,5 +1,34 @@
 package com.pedidosms.pedidos.entity;
 
+import com.pedidosms.pedidos.enums.SituacaoPedido;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Data
+@Builder
+@Table(name = "PEDIDOS")
+@AllArgsConstructor @NoArgsConstructor
+@Getter
+@Setter
 public class Pedidos {
+	
+	@Id
+	@Column(name = "PEDIDOS_ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private SituacaoPedido situacaoPedido;
+	private Long clienteId;
 
 }
