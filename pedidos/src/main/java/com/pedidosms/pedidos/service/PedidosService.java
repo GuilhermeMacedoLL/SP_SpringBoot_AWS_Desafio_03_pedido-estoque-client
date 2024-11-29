2320a9f7eab5bc3d97451fbf3d7f611df07ae1ea
Guilherme Macedo <macedo.lac157@gmail.com>
2024-11-29 17:45:50 -0300

    Mapeamento da interface PedidosService

--- pedidos/src/main/java/com/pedidosms/pedidos/service/PedidosService.java
+++ pedidos/src/main/java/com/pedidosms/pedidos/service/PedidosService.java
@@ -1,5 +1,20 @@
 package com.pedidosms.pedidos.service;
 
-public class PedidosService {
+import java.util.List;
+
+import com.pedidosms.pedidos.dto.PedidosDTO;
+import com.pedidosms.pedidos.entity.Pedidos;
+
+public interface PedidosService {
+	
+	List<Pedidos> getAll();
+	
+	Pedidos getPedidos(Long id);
+	
+	Pedidos create(PedidosDTO dtoInput);
+	
+	Pedidos update(PedidosDTO dtoInput);
+	
+	void delete(Long id);
 
 }
