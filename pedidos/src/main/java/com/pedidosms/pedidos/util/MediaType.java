881f7b95119d062be7e10568ef4b84c6a350db9a
Guilherme Macedo <macedo.lac157@gmail.com>
2024-11-29 18:08:52 -0300

    MediaType mapeado

--- pedidos/src/main/java/com/pedidosms/pedidos/util/MediaType.java
+++ pedidos/src/main/java/com/pedidosms/pedidos/util/MediaType.java
@@ -1,5 +1,8 @@
 package com.pedidosms.pedidos.util;
 
 public class MediaType {
+	
+	public static final String APPLICATION_JSON = "application/json";
+	public static final String APPLICATION_XML = "application/xml";
 
 }
