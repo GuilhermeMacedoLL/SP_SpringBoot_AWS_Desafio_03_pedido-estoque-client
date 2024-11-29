e95dc6056dc616961f26d89e5d619d61bb5052c9
Guilherme Macedo <macedo.lac157@gmail.com>
2024-11-29 18:07:37 -0300

    Classe controller mapeada

--- pedidos/src/main/java/com/pedidosms/pedidos/controller/PedidosController.java
+++ pedidos/src/main/java/com/pedidosms/pedidos/controller/PedidosController.java
@@ -1,5 +1,70 @@
 package com.pedidosms.pedidos.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.tomcat.util.http.parser.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pedidosms.pedidos.dto.PedidosDTO;
+import com.pedidosms.pedidos.mapper.PedidosMapper;
+import com.pedidosms.pedidos.service.PedidosService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/pedidos")
 public class PedidosController {
+
+	@Autowired
+	private PedidosService service;
+
+	@Autowired
+	private PedidosMapper mapper;
+
+	@GetMapping(value = "/consulta", produces = MediaType.APPLICATION_JSON)
+	public ResponseEntity<List<PedidosDTO>> getAll() {
+		List<PedidosDTO> listaDto = new ArrayList<>();
+		listaDto = this.mapper.convertListEntityToListDto(this.service.getAll());
+		return new ResponseEntity<List<PedidosDTO>>(listaDto, HttpStatus.OK);
+	}
+
+	@GetMapping(value = "/consulta/{id}", produces = MediaType.APPLICATION_JSON)
+	public ResponseEntity<PedidosDTO> getNome(@PathVariable Long id) {
+		PedidosDTO dto = new PedidosDTO();
+		dto = this.mapper.convertEntityToDto(this.service.getPedidos(id));
+		return new ResponseEntity<PedidosDTO>(dto, HttpStatus.OK);
+	}
+
+	@PostMapping(value = "/salvar", produces = MediaType.APPLICATION_JSON, consumes = MediaType.APPLICATION_JSON)
+	public ResponseEntity<PedidosDTO> create(@RequestBody PedidosDTO dtoInput) {
+		PedidosDTO dto = new PedidosDTO();
+		dto = this.mapper.convertEntityToDto(this.service.create(dtoInput));
+		return new ResponseEntity<PedidosDTO>(dto, HttpStatus.CREATED);
+	}
+
+	@PutMapping(value = "/alterar", produces = MediaType.APPLICATION_JSON, consumes = MediaType.APPLICATION_JSON)
+	public ResponseEntity<PedidosDTO> update(@RequestBody PedidosDTO dtoInput) {
+		PedidosDTO dto = new PedidosDTO();
+		dto = this.mapper.convertEntityToDto(this.service.update(dtoInput));
+		return new ResponseEntity<PedidosDTO>(dto, HttpStatus.NO_CONTENT);
+	}
+
+	@SuppressWarnings("rawtypes")
+	@GetMapping(value = "/excluir/{id}")
+	public ResponseEntity delete(@PathVariable Long id) {
+		service.delete(id);
+		return new ResponseEntity(HttpStatus.NO_CONTENT);
+	}
 
 }
