bdbcfe447af6712dea786fb6c8595126f9661318
Guilherme Macedo <macedo.lac157@gmail.com>
2024-11-28 23:56:35 -0300

    Criação da conexão com Open Feign

--- cliente/src/main/java/com/clientems/cliente/config/OpenFg.java
+++ cliente/src/main/java/com/clientems/cliente/config/OpenFg.java
@@ -1,5 +1,14 @@
 package com.clientems.cliente.config;
 
-public class OpenFg {
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.clientems.cliente.dto.ClientesDTO;
+
+@FeignClient(value = "OpenFg-Cliente", url = "http://localhost:8082/pedido")
+public interface OpenFg {
+
+	@PostMapping(value = "/consulta")
+	public ClientesDTO clientePedidoRetorno(ClientesDTO dto); 
+	
 }
