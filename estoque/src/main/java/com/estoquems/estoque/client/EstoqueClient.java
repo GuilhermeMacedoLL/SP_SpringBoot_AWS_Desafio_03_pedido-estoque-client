3377fe208e65575aefbb02fd2c9edf5816854828
Guilherme Macedo <macedo.lac157@gmail.com>
2024-12-01 01:38:09 -0300

    Correções dentro de EstoqueClient

--- estoque/src/main/java/com/estoquems/estoque/client/EstoqueClient.java
+++ estoque/src/main/java/com/estoquems/estoque/client/EstoqueClient.java
@@ -1,5 +1,18 @@
 package com.estoquems.estoque.client;
 
-public class EstoqueClient {
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.estoquems.estoque.util.MediaType;
+
+@Component
+@FeignClient(value = "pedidos", url= "localhost:8081", path = "/pedidos")
+public interface EstoqueClient {
+	
+	@GetMapping(value = "/consulta/{id}", produces = MediaType.APPLICATION_JSON)
+	public ResponseEntity<PedidosDTO> getId(@PathVariable Long id);
 
 }
