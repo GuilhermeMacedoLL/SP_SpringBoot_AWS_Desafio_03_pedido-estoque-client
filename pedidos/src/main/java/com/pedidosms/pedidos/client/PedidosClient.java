615f96cbf6368a3dc3aa1b37262670e8f672041f
Guilherme Macedo <macedo.lac157@gmail.com>
2024-12-01 00:02:11 -0300

    Mapeamento da classe PedidosClient

--- pedidos/src/main/java/com/pedidosms/pedidos/client/PedidosClient.java
+++ pedidos/src/main/java/com/pedidosms/pedidos/client/PedidosClient.java
@@ -1,5 +1,19 @@
 package com.pedidosms.pedidos.client;
 
-public class PedidosClient {
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.pedidosms.pedidos.dto.PedidosDTO;
+import com.pedidosms.pedidos.util.MediaType;
+
+@Component
+@FeignClient(name = "cliente", url = "localhost:8080", path = "/clientes")
+public interface PedidosClient {
+	
+	@GetMapping(value = "/consulta/{id}", produces = MediaType.APPLICATION_JSON)
+	public ResponseEntity<ClientesDTO> getNome(@PathVariable Long id);
 
 }
